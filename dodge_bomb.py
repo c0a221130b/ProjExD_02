76537d76aa407ddca67a34b2d755ef335a0dcaab
C0A22113 <c0a221130b@edu.teu.ac.jp>
2023-11-28 14:35:25 +0900

    練習2, 3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import random
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {  # 練習3
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0)
+    }
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -11,12 +17,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))  # 練習1 透明のsurfaceをつける
     bb_img.set_colorkey(0,0)
     pg.draw.circle(bb_img, (255, 0, 0), (10,10), 10)
     bb_rct = bb_img.get_rect()  # 爆弾SurfaceのRectを抽出する
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, WIDTH)
+    vx, vy = +5, +5
     
     clock = pg.time.Clock()
     tmr = 0
@@ -25,12 +34,22 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        key_lst = pg.key.get_pressed()
+        sum_move = [0, 0]
+        for k, tpl in delta.items():
+            if key_lst[k]:  # キーが押されたら
+                sum_move[0] += tpl[0]
+                sum_move[1] += tpl[1]
+                
+        screen.blit(bg_img, [0, 0])  #背景
+        kk_rct.move_ip(sum_move[0], sum_move[1])  # 練習3 こうかとん移動
+        screen.blit(kk_img, kk_rct)  # こうかとんをblit
+        bb_rct.move_ip(vx, vy)  # 練習2
         screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
